c5458917912935308e7d9a229edc6bc34070f052
stanislav <stanislav.vasilchenko@yandex.ru>
2024-09-03 22:01:30 +0300

    some refactoring

--- second_task/views.py
+++ second_task/views.py
@@ -8,17 +8,18 @@ from second_task.models import PlayerLevel, LevelPrize
 def export_csv(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="player_level.csv"'
-
     writer = csv.writer(response)
-    writer.writerow(['Player ID', 'Level Name', 'Level completed', 'Prize Tittle',])
-
+    writer.writerow(['Player ID', 'Level Name', 'Level completed', 'Prize Tittle', ])
     player_levels = PlayerLevel.objects.select_related('player', 'level').prefetch_related('level__levelprize_set')
+
+    if not player_levels:
+        return HttpResponse(content='No players')
 
     for player_level in player_levels:
         prize_title = None
         try:
             level_prize = LevelPrize.objects.get(level=player_level.level)
-            prize_title = level_prize.title
+            prize_title = level_prize.prize.title
         except ObjectDoesNotExist:
             prize_title = "No Prize"
 
@@ -28,7 +29,4 @@ def export_csv(request):
             player_level.is_completed,
             prize_title
         ])
-
         return response
-
-
